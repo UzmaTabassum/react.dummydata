b1020d8cd4312a65c5effdeabdbd94449347560a
UzmaTabassum <uzmatabassumshaik@gmail.com>
2023-10-16 09:40:21 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import Table from './components/Table';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Table/>
     </div>
-  );
+    );
 }
 
 export default App;
+
+/*
+echo "# ReactJSTask-4" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Raaj1500s/ReactJSTask-4.git
+git push -u origin main
+*/
